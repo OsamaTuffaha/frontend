90ebd1be8751014413f83dd3dfe435eeadda3bcd
Osama Tuffaha <osamamohdtuffaha@gmail.com>
2026-03-27 19:42:40 +0300

    osama

--- src/components/services/loginApi.jsx
+++ src/components/services/loginApi.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API = import.meta.env.VITE_API_URL;
+
 export const loginApi = async (email, password) => {
-  const result = await axios.post("http://localhost:5000/user/login", {
+  const result = await axios.post(`${API}/user/login`, {
     email,
     password,
   });
